f965dfd627f5a21332898289713d1c53b8d8b79a
Henry Bravo <henry.bravo89@gmail.com>
2019-01-22 06:24:11 -0500

    Sections descriptions

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 (function() {
 
-    var pointer = document.getElementsByClassName('pointer');
-    var showPointer = true;
-
-    // for(var i = 0; i < divsToHide.length; i++){
-    //     divsToHide[i].style.visibility = "hidden"; // or
-    //     divsToHide[i].style.display = "none"; // depending on what you're doing
-    // }
+    var pointers = document.getElementsByClassName('pointer');
+    var showPointers = [];
+    for(var i = 0; i < pointers.length; i++){
+        showPointers[i]=false;
+    }
     setInterval(()=>{
-        if(showPointer){
-            pointer[0].style.display = "none";
-            showPointer = false;
-        }else{
-            pointer[0].style.display = "block";
-            showPointer = true;
+        for(var i = 0; i < pointers.length; i++){
+            // divsToHide[i].style.visibility = "hidden"; // or
+            // divsToHide[i].style.display = "none"; // depending on what you're doing
+            if(showPointers[i]){
+                pointers[i].style.display = "none";
+                showPointers[i] = false;
+            }else{
+                pointers[i].style.display = "block";
+                showPointers[i] = true;
+            }
         }
     }, 600);
+    
  
  })();
